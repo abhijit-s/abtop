12e0568a5d29d5d53006957e9ca408663e489c0f
Abhijit Salvi <a.salvi@easygo.io>
2026-06-10 22:25:36 +1000

    chore(release): update Formula to v0.6.0

--- Formula/abtop.rb
+++ Formula/abtop.rb
@@ -1,25 +1,25 @@
 class Abtop < Formula
   desc "AI agent monitor for your terminal"
   homepage "https://github.com/abhijit-s/abtop"
-  version "0.5.1"
+  version "0.6.0"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/abhijit-s/abtop/releases/download/v0.5.1/abtop-aarch64-apple-darwin.tar.xz"
-      sha256 "7c83d11117e6046993519e7d0c0fbb7c50907d820ed66858648bce486c869ec0"
+      url "https://github.com/abhijit-s/abtop/releases/download/v0.6.0/abtop-aarch64-apple-darwin.tar.xz"
+      sha256 "49b4f13b0206a9dc3317da74c9a3d83ba0f2ab949f041ee3f156ba358722c06f"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/abhijit-s/abtop/releases/download/v0.5.1/abtop-x86_64-apple-darwin.tar.xz"
-      sha256 "49c473f42f9578b9ffe3235eeb1f2f0fbd681671865430529e7c94e2a1c5583a"
+      url "https://github.com/abhijit-s/abtop/releases/download/v0.6.0/abtop-x86_64-apple-darwin.tar.xz"
+      sha256 "7c96711ddd27826102a94068a32cd67a4bb40964951e0eaafb0dd0204cbe2b55"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/abhijit-s/abtop/releases/download/v0.5.1/abtop-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "1807825e496e64bf2e60071aaf2f909ce7ecac84c76c8d0e314156015790e012"
+      url "https://github.com/abhijit-s/abtop/releases/download/v0.6.0/abtop-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "c9f9728c0b6b1ec510fdf4246f01dabfb16bc3f8e7d4686da3a8a053ef3b7079"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/abhijit-s/abtop/releases/download/v0.5.1/abtop-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "ca36364685d182d8b349684559b77f96fc94d485ca7f1e58875717035f119b3d"
+      url "https://github.com/abhijit-s/abtop/releases/download/v0.6.0/abtop-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "a8711c5b0c755e32df472c714aa04ced9958943393931e45e8a929342ab4dc7b"
     end
   end
   license "MIT"
